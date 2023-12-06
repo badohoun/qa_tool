2a2814701e30fa7629528c7200650da14b2f1f74
e-badohoun <e-badohoun@autodistribution.com>
2023-12-06 16:45:35 +0100

    fix openapi key v2

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
 
 def doc_preprocessing():
